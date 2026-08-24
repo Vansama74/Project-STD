63ea041027277fdfc4248dff77720fdb2e0052ed
Vansama74 <Vansama74@users.noreply.github.com>
2026-08-24 17:28:38 +0800

    chore: PROGRAM_CODE 更新为 9K1F3127E2，workspace 调试变量格式改为自然格式

--- Application/Inc/app_boot.h
+++ Application/Inc/app_boot.h
@@ -2,12 +2,11 @@
  * @file    app_boot.h
  * @brief   系统启动编排器接口
  *
- * PROGRAM_CODE 为产品程序编码：开机画面（FW 行）与协议版本应答
- * （山东协议 '2' 取版本号）共用同一常量。
+ * PROGRAM_CODE 为产品程序编码：开机画面与协议版本应答
  */
 
 #pragma once
 
-#define PROGRAM_CODE "9K10212482"
+#define PROGRAM_CODE "9K1F3127E2"
 
 void app_boot(void);
